be6233d0992cdbd8e39d4d2d4cc855389a13ed31
Sumitra Kumari Shaw <sumitrakumarishaw1@gmail.com>
2022-11-27 13:46:17 +0000

    Added solution - LeetHub

---
+++ Binary Search - GFG/binary-search.js
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(
+        string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res, n) {
+    let s = "";
+    for (let i = 0; i < n; i++) {
+        s += res[i];
+        s += " ";
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x => parseInt(x));
+        for (let i = 0; i < n; i++) {
+            arr[i] = input_ar1[i];
+        }
+        let k = parseInt(readLine());
+        let obj = new Solution();
+        let res = obj.binarysearch(arr, n, k);
+        console.log(res);
+    }
+} // } Driver Code Ends
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @param {number} k
+ * @returns {number}
+*/
+
+class Solution {
+    binarysearch(arr, n, x) {
+        // code here
+        let l = 0;
+    let r = arr.length - 1;
+    let mid;
+    while (r >= l) {
+         mid = l + Math.floor((r - l) / 2);
+  
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+  
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            r = mid - 1;
+             
+        // Else the element can only be present
+        // in right subarray
+        else
+            l = mid + 1;
+    }
+  
+    // We reach here when element is not
+    // present in array
+    return -1;
+    }
+}
